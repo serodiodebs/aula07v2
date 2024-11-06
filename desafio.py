2a044a6a75689543645f31bb0ba377c87a4bc878
serodiodebs <32377390+serodiodebs@users.noreply.github.com>
2024-11-05 22:14:13 -0300

    feat: criar funcao que calcula valores

--- desafio.py
+++ desafio.py
@@ -27,12 +27,23 @@ def processar_dados(lista_dicionario: list[dict]) -> dict:
     return dicionario_produto
 
 def calcular_vendas_categoria(dicionario_entrada: dict) -> dict:
+    dicionario_final = {}
+
     for c,v in dicionario_entrada.items():
-        for item in v:
-            print(f"valor da chave: {c} é: {item}")
+        total_vendas = 0
+
+        for produto in v:
+            quantidade = int(produto[1])
+            valor = int(produto[2])
+
+            total_vendas += quantidade * valor
+
+        dicionario_final[c] = total_vendas
+
+    return dicionario_final
 
 
 
 dados_funcao1 = ler_csv(nome_arquivo)
 dados_funcao2 = processar_dados(dados_funcao1)
-calcular_vendas_categoria(dados_funcao2)
+print(calcular_vendas_categoria(dados_funcao2))
